84f0781a11278f11eeadf3c3e63f3e4630406c0d
Ccode-400 <stanleymngige@gmail.com>
2026-05-13 00:31:16 +0300

    added imports from components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import './App.css'
+import HomePage from "./pages/HomePage";
+import AdoptNow from "./pages/AdoptNow";
+import Volunteer from "./pages/Volunteer";
+import DonateNow from "./pages/DonateNow";
+import ReportCase from "./pages/ReportCase";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
+
 
 function App() {
   
