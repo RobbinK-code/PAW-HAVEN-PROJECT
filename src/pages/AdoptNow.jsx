2bd580e0d1afb6c215fdeeffad7bfbc3a637a9e0
Ccode-400 <stanleymngige@gmail.com>
2026-05-14 14:47:49 +0300

    add useEffect to fetch pets

--- src/pages/AdoptNow.jsx
+++ src/pages/AdoptNow.jsx
@@ -5,5 +5,13 @@ import PetForm from "../components/PetForm";
 function AdoptNow() {
   const [pets, setPets] = useState([]);
 
+  // Fetch pets from db.json
+  useEffect(() => {
+    fetch("http://localhost:3000/pets")
+      .then((res) => res.json())
+      .then((data) => setPets(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   
 }
