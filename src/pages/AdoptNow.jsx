45d797b4bd2d4dbc1d6c6a400ceff828d03e4f5b
Ccode-400 <stanleymngige@gmail.com>
2026-05-14 14:46:31 +0300

    add AdoptNow function and variables

--- src/pages/AdoptNow.jsx
+++ src/pages/AdoptNow.jsx
@@ -2,3 +2,8 @@ import { useEffect, useState } from "react";
 import PetCard from "../components/PetCard";
 import PetForm from "../components/PetForm";
 
+function AdoptNow() {
+  const [pets, setPets] = useState([]);
+
+  
+}
