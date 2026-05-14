c7ff3219dff2338856b2579a0bc7dec4e9cfa60c
Ccode-400 <stanleymngige@gmail.com>
2026-05-14 15:10:28 +0300

    add state for pet adopt form fields

--- src/components/PetForm.jsx
+++ src/components/PetForm.jsx
@@ -1,2 +1,22 @@
 import { useState } from "react";
 import "../styles/PetForm.css";
+
+function PetForm({ onAddPet }) {
+  const [formData, setFormData] = useState({
+    image_url: "",
+    pet_name: "",
+    pet_type: "",
+    breed: "",
+    age: "",
+    gender: "",
+    vaccinated: false,
+    spayed_neutered: false,
+    adoption_fee: "",
+    adoption_date: "",
+    owner_name: "",
+    owner_email: "",
+    owner_address: "",
+  });
+
+  
+}
